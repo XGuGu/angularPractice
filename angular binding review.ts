5e2752d1c4c7bd471746bc5656ffd20aa4db7b82
lingjie zhang <zhanglingjie16303@gmail.com>
2019-06-24 23:05:13 -0400

    binding 0624

--- angular binding review.ts
+++ angular binding review.ts
@@ -15,12 +15,65 @@ example:
   selector: 'app-test',
   template"`
     <input [id]="myId" >
+    <input id="{{myId}}" > //also fine ,however, interpolation can only work with String values
+    <input [disabled]="isDisabled" >
+    <input bind-disabled="isDisabled" >
   `,
   styles:[]
 })
 
 export class TestComponent implements OnInit {
   public myId = "testId";
+  public isDisabled = false;
 
   ngOnInit() {}
+}
+
+Binding Classes
+
+// class binding is useful allows you to dynamiclly add or remove classes
+// based on your application states
+
+template: `
+  <h2 [class]="myClass" >
+  <h2 [class.text-danger]="hasError" >
+
+  <h2 [ngClass]="messageClasses" >
+`
+styles:[
+  .text-danger {
+    color:red;
+  }
+  .text-success {
+    color: green;
+  }
+  .text-special {
+    font:italic;
+  }
+]
+
+public hasError = true;
+public isSpecial = true;
+public messageClasses = {
+  "text-sucess": !this.hasError,
+  "text-danger": this.hasError,
+  "text-special": this.isSpecial
+}
+
+
+Style Binding
+
+template: `
+  <h2 [style.color]="'orange'" >
+  <h2 [style.color]="hasError ? 'red' : 'green'"
+  <h2 [style.color]="highlightColor" >
+
+  <h2 [ngStyle]="titleStyles" >
+`
+
+public hasError = true;
+public highlightColor = "orange";
+public titleStyles = {
+  color: "blue",
+  fontStyle: "italic"
 }
