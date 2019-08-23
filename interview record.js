da2913d455bad595b0c8608d50a5b641c50962df
lingjie zhang <zhanglingjie16303@gmail.com>
2019-08-23 15:02:06 -0400

    0823 interview

---
+++ interview record.js
@@ -0,0 +1,14 @@
+// 0823
+       what library ? 3d js ?
+       features you have built?
+       what is most complex feature you have built?
+       which angular version is your strongest?
+       Have you used angular 4?
+       What about angular JS ?
+       react -redux experience?
+       what library have you used in your project ?
+       any UI components have you built?
+       Any unit test experience ?
+       What library have you used for unit test?
+       Have experience with Jasmine?
+       algorithm problem: return the diagnoal elements contains the number in 2d array.
