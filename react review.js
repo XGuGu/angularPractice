7f6301fb040d20520ea77653db1208fb70a9fea8
lingjie zhang <zhanglingjie16303@gmail.com>
2019-08-22 21:56:21 -0400

    react review

--- react review.js
+++ react review.js
@@ -23,10 +23,15 @@ class ClickCounter extends React.Component {
     this.state = {count: 0}
   }
 
+  click(event) {
+    event.preventDefault();
+    this.setState({ count: this.state.count + 1 });
+  }
+
   render() {
     return (
       <div>
-        <button> CLick me !! </button>
+        <button onClick={this.click.bind(this)}> CLick me !! </button>
         <span>{this.state.count}</span> //the curely braces is for interpolation
       </div>
     )
@@ -46,3 +51,77 @@ Components: Render
 7.  must return a single React element
       single element can have any number of descendants
     this has a single root: the <div>
+
+
+Components: Events
+1.  Handler set by passing prop: onClick, onChange, onWhaterverTheEventNameIs
+2.  Value is pointer to handler function
+3.  Event handler function is passed event object, just like jQuery event
+4.  Event object contains target, current target, time, mouse location, etc.
+      almost always we will preventDefault()
+
+Rendering Components in the Browser
+1. Need to wait until DOM has loaded so we have a target to render into
+2. ReactDOM.render will replace entire content of target HTML element
+
+
+
+Transpilation
+Browser cannot natively understand; must be transiled into raw JS
+
+Babel /JSX Transpilation
+
+React: Functional Components
+
+// a click counter with reset button
+import React from 'react'
+
+class ClickCounter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {count: 0, previousCounts: [] }
+  }
+
+  click(event) {
+    event.preventDefault();
+    this.setState({ count: this.state.count + 1 });
+  }
+
+  reset(event) {
+    event.preventDefault();
+    const previousCounts = this.state.previousCounts;
+    previousCounts.push(this.state.count);
+    this.setState({ count: 0, previousCounts: previousCounts });
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.click.bind(this)}> CLick me !! </button>
+        <span>{this.state.count}</span> //the curely braces is for interpolation
+        <button onClick={this.reset.bind(this)}> Reset! </button>
+        <ul>
+          {
+            this.state.previousCounts.map(count => <li>{count}</li>)
+          }
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default ClickCounter;
+
+
+// or write a counts component !!!
+<Counts previousCounts={this.state.previousCounts} />
+
+const Counts = (props) => (
+  <ul>
+    {
+      props.previousCounts.map((count, idx) => <li key={idx}>{count}</li>)
+    }
+  </ul>
+)
+
+export default Counts; //this is a purely functional component
