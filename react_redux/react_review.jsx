6f9c1a01d7ba69f2abbd84f3cab72ab0e7c73454
lingjie zhang <zhanglingjie16303@gmail.com>
2019-08-30 15:10:17 -0400

    react review

---
+++ react_redux/react_review.jsx
@@ -0,0 +1,16 @@
+//Inline styling with JSX
+HTML:
+  <div style="background-color: red;"></div>
+  <div style="background-color: red; color: white;"></div>
+  <div style="border: 1px solid red;"></div>
+JSX:
+  <div style={{ backgroundColor: 'red' }}></div>
+  <div style={{ backgroundColor: 'red', color: 'white' }}></div>
+  <div style={{ border: '1px solid red' }}></div>
+    //single qoute or double qoutes are both fine
+
+// class vs className
+  we use className to aviod colision with class
+  like class App extends React.Component {}....
+
+ 
